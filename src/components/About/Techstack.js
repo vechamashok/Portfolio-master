9ae9946e9d5df189881990be8900cf039a673c29
vechamashok <vechamashok@users.noreply.github.com>
2022-04-29 23:31:43 -0400

    FirstCommit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJava,
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiSpring,
+  SiPytorch,
+  SiFirebase,
+  SiNextdotjs,
+  SiSpringboot,
+  SiJenkins,
+  SiKubernetes,
+  SiDocker,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpring />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpringboot />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJenkins />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKubernetes />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { DiEclipse } from "react-icons/di";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+  SiIntellijidea,
+  SiSpring,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiEclipse />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiIntellijidea />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpring />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
