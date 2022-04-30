199d5006ed1176c3bbfeea5fae0f8b79f939f5d4
vechamashok <vechamashok@users.noreply.github.com>
2022-04-29 23:34:07 -0400

    Resume link Update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/vechamashok/portfolio/master/src/Assets/ASHOKGUPTA V A - Resume.pdf";
+  "https://raw.githubusercontent.com/vechamashok/portfolio-master/main/src/Assets/ASHOKGUPTA V A - Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
